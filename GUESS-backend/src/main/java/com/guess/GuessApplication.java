b029f0e4e0f009144336f1333209107d374ab1c2
VladMigura <vlad_migura@mail.ru>
2019-02-19 22:23:20 +0300

    Initial commit.

---
+++ GUESS-backend/src/main/java/com/guess/GuessApplication.java
@@ -0,0 +1,13 @@
+package com.guess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessApplication.class, args);
+    }
+
+}
