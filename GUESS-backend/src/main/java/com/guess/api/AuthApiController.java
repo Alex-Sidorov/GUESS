cd1f467b0752098c6ea8074f5599a15d35df7ea5
vladmigura <vladislav.migura@yellow.id>
2019-02-23 14:15:20 +0300

    Described base API structure.

---
+++ GUESS-backend/src/main/java/com/guess/api/AuthApiController.java
@@ -0,0 +1,28 @@
+package com.guess.api;
+
+import com.guess.controller.AuthApi;
+import com.guess.model.SigninRequest;
+import com.guess.model.SignupRequest;
+import com.guess.model.TokenModel;
+import com.guess.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@AllArgsConstructor
+public class AuthApiController implements AuthApi {
+
+    @Override
+    public ResponseEntity<User> signUp(@Valid SignupRequest signupRequest) {
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<TokenModel> signIn(@Valid SigninRequest signinRequest) {
+        return null;
+    }
+}
